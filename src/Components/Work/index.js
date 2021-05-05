26d376a90af51c8dc4ec546b3c673aca22a2655b
Alwyn-Ong <52643926+Alwyn-Ong@users.noreply.github.com>
2021-05-05 15:32:03 +0800

    Added animations

--- src/Components/Work/index.js
+++ src/Components/Work/index.js
@@ -1,24 +1,27 @@
 import { Avatar, Grid } from "@material-ui/core";
 import React, { Component } from "react";
-import "./styles.css"
+// import "./styles.css";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const Resume = (props) => {
   if (props.data) {
     var work = props.data.work.map(function (work) {
       return (
-        <div key={work.company} className="company">
-              <div className="company-logo">
-                <img src="/images/logo_govtech_hort.gif" />
-              </div>
-              <h3>{work.company}</h3>
-              <p className="info">
-                {work.title}
-                <span>&bull;</span> <em className="date">{work.years}</em>
-              </p>
-              {work.description.map((activity, i) => {
-                return <li key={i}>{activity}</li>;
-              })}
-              <br></br>
+        <div key={work.company}>
+          <ScrollAnimation animateIn="fadeInDown">
+          <div className="company-logo">
+            <img src={`/images/${work.logo}`} />
+          </div>
+            <h3>{work.company}</h3>
+            <p className="info">
+              {work.title}
+              <span>&bull;</span> <em className="date">{work.years}</em>
+            </p>
+            {work.description.map((activity, i) => {
+              return <li key={i}>{activity}</li>;
+            })}
+            <br></br>
+          </ScrollAnimation>
         </div>
       );
     });
